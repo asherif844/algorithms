08099f3dd1fa97a6bac6d69e5efad673153e3f11
Ahmed Sherif <Ahmed.sherif@att.net>
2021-04-27 12:23:41 -0400

    first non repeating character

--- firstNonRepeatingCharacter/exercise1.py
+++ firstNonRepeatingCharacter/exercise1.py
@@ -1,7 +1,26 @@
 def firstNonRepeatingCharacter(string):
-    # Write your code here.
-    return -1
+    try:
+        wordCount = obtainDictionary(string)
+        word = list(wordCount.keys())[list(wordCount.values()).index(0)]
+        index = list(string).index(word)
+        return index
+    except:
+        return -1
 
 
-string = 'abcdcaf'
+def obtainDictionary(string):
+    wordCount = {}
+    count = 0
+    for idx in range(len(string)):
+        if string[idx] not in wordCount:
+            wordCount[string[idx]] = 0
+        else:
+            wordCount[string[idx]] += 1
+    return wordCount
+
+
+def getFirstLetter(wordCount):
+    return list(x for x in wordCount.keys() if x == '0')
+
+string = 'faadabcbbebdfc'
 firstNonRepeatingCharacter(string)
